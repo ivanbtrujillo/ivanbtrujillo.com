19c1917d13e209040c6935d74a6842e7c125a2a2
ivanbtrujillo <ivanbtrujillo@gmail.com>
2020-04-28 17:42:56 +0100

    update(auth0 config)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -62,7 +62,7 @@ export default ({ Component, pageProps }: AppProps) => (
   <Auth0Provider
     domain={process.env.AUTH0_DOMAIN}
     clientId={process.env.AUTH0_CLIENT_ID}
-    redirectUri="http://ivanbtrujillo.com"
+    redirectUri={process.env.AUTH0_REDIRECT_URI}
     onLoginError={onLoginError}
     onAccessTokenError={onAccessTokenError}
     onRedirecting={onRedirecting}
