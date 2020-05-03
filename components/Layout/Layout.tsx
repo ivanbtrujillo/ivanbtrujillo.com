a469444b4394c55ed2c69eaa1b2cdfeb107182ee
ivanbtrujillo <ivanbtrujillo@gmail.com>
2020-05-03 20:06:35 +0100

    update(og image): secure url

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -36,6 +36,11 @@ export const Layout: React.SFC<LayoutProps> = ({ children, title }) => {
             content="https://og-image.now.sh/Ivan%20Trujillo.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-white-logo.svg&images=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F1254434620894044161%2F1-1_PQWI_400x400.jpg"
           />
 
+          <meta
+            property="og:image:secure_url"
+            content="https://og-image.now.sh/Ivan%20Trujillo.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-white-logo.svg&images=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F1254434620894044161%2F1-1_PQWI_400x400.jpg"
+          />
+
           <meta property="twitter:card" content="summary_large_image" />
           <meta property="twitter:url" content="https://ivanbtrujillo.com/" />
           <meta property="twitter:title" content={`${title} - ${siteTitle}`} />
