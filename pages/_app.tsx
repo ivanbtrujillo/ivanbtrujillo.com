c027928aa28b88222a89e80daef83a523bcd9cd2
ivanbtrujillo <ivanbtrujillo@gmail.com>
2020-05-23 18:37:00 +0100

    fix(app): return

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,16 +17,17 @@ export default ({ Component, pageProps }: AppProps) => {
 
   if (!isMounted) return <p>Cargando ...</p>;
 
-  return;
-  <Auth0Provider
-    domain={process.env.AUTH0_DOMAIN}
-    clientId={process.env.AUTH0_CLIENT_ID}
-    redirectUri={process.env.AUTH0_REDIRECT_URI}
-    onLoginError={onLoginError}
-    onAccessTokenError={onAccessTokenError}
-    onRedirecting={onRedirecting}
-    onRedirectCallback={onRedirectCallback}
-  >
-    <Component {...pageProps} />
-  </Auth0Provider>;
+  return (
+    <Auth0Provider
+      domain={process.env.AUTH0_DOMAIN}
+      clientId={process.env.AUTH0_CLIENT_ID}
+      redirectUri={process.env.AUTH0_REDIRECT_URI}
+      onLoginError={onLoginError}
+      onAccessTokenError={onAccessTokenError}
+      onRedirecting={onRedirecting}
+      onRedirectCallback={onRedirectCallback}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  );
 };
