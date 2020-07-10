8339f3debd48494a80d92e91ac28d069f0767be0
ivanbtrujillo <ivanbtrujillo@gmail.com>
2020-07-10 17:34:50 +0100

    update(theme): colors to make text readable (accessibility)

--- components/PostDate/PostDate.tsx
+++ components/PostDate/PostDate.tsx
@@ -9,7 +9,7 @@ type PostDateProps = {
 export const PostDate: React.SFC<PostDateProps> = ({ dateString }) => {
   const date = parseISO(dateString);
   return (
-    <div className="flex flex-row text-font-secondary">
+    <div className="flex flex-row text-font-primary">
       <FiCalendar className="ml-1 mr-1 mt-1" />
 
       <time dateTime={dateString}>
